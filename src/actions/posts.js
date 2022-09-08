a4be93565427406be38775a587bc2f32496b70dc
Priyanshu <princepriyanshu.vaishnav@gmail.com>
2022-09-08 11:33:39 +0530

    CRUD OPERATION DONE

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -8,14 +8,13 @@ export const getPosts = () => async (dispatch) => {
 
         dispatch({ type: 'FETCH_ALL', payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
-
         dispatch({ type: 'CREATE', payload: data });
     } catch (error) {
         console.log(error);
@@ -29,6 +28,25 @@ export const updatePost = (id, post) => async (dispatch) => {
 
         dispatch({ type: 'UPDATE', payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type: 'DELETE', payload: id });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+        dispatch({ type: 'LIKE', payload: data });
+    } catch (error) {
+        console.log(error);
     }
 }
